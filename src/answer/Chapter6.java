787287095b3d741a8715ab4150a33ee02c6d1d3a
amjt2 <amjt2@192.168.2.116>
2023-07-09 23:28:36 +0900

    chapter6の作成

---
+++ src/answer/Chapter6.java
@@ -0,0 +1,13 @@
+package answer;
+
+import comment.Zenhan;
+
+public class Chapter6 {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+
+}
